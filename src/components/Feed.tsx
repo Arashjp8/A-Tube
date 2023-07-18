309cec973169514c93c4970e5bf9c6816976c666
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-18 15:42:31 +0330

    videocard added

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,16 @@
 import SideBar from "./SideBar";
+import VideoCard from "./VideoCard";
 
 const Feed = () => {
   return (
-    <div>
+    <div className="flex">
       <SideBar />
+      <div className="w-full text-mainWhite grid grid-cols-4 gap-5 px-10">
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+      </div>
     </div>
   );
 };
---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,33 @@
+import {
+  demoChannelTitle,
+  demoProfilePicture,
+  demoThumbnailUrl,
+  demoVideoTitle,
+} from "../utils/constans";
+
+const VideoCard = () => {
+  return (
+    <div className="cursor-pointer h-[340px] py-2 flex flex-col items-center">
+      <img
+        src={demoThumbnailUrl}
+        alt="thumbnail"
+        className="hover:rounded-none rounded-2xl h-[200px]"
+      />
+      <div className="flex flex-row p-5">
+        <img
+          src={demoProfilePicture}
+          alt="profile-picture"
+          className="w-[32px] h-[32px] rounded-full mr-3"
+        />
+        <span>
+          <h4 className="text-mainWhite">{demoVideoTitle}</h4>
+          <p className="hover:text-mainWhite text-mainGray">
+            {demoChannelTitle}
+          </p>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
