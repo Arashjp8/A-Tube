36c94167106ae7ed1663776715f6418f61bb1f25
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-25 12:38:30 +0330

    searchfeed load and error handling

--- src/pages/SearchFeed.tsx
+++ src/pages/SearchFeed.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import SearchFeedVideoCard from "../components/SearchFeedVideoCard";
 import { divStyle, noToggleDivStyle, toggleDivStyle } from "./Feed";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import fetchFromAPI from "../utils/fetchFromAPI";
 import { VideoProps } from "../interfaces/Video";
 
@@ -22,12 +22,47 @@ const SearchFeed = ({
 }: Props) => {
   const { searchTerm } = useParams();
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
-      setVideos(data.items)
-    );
-    console.log(searchTerm);
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const data = await fetchFromAPI(`search?part=snippet&q=${searchTerm}`);
+
+        setVideos(data.items);
+        setIsLoading(false);
+      } catch (error: any) {
+        setError(error.response.status);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
   }, [searchTerm]);
+
+  if (isLoading)
+    return (
+      <h2
+        className={`${
+          toggle ? `${toggleDivStyle}` : `${noToggleDivStyle}`
+        } ${divStyle} font-semibold text-3xl my-5 px-10 h-[100vh]`}
+      >
+        Loading...
+      </h2>
+    );
+
+  if (error) {
+    return (
+      <h2
+        className={`${
+          toggle ? `${toggleDivStyle}` : `${noToggleDivStyle}`
+        } ${divStyle} font-semibold text-3xl my-5 px-10 h-[100vh]`}
+      >
+        {error}
+      </h2>
+    );
+  }
 
   return (
     <>
@@ -35,7 +70,7 @@ const SearchFeed = ({
         className={`${
           toggle
             ? `${toggleDivStyle} lg:mx-[17%]`
-            : `${noToggleDivStyle} lg:mx-[20%]`
+            : `${noToggleDivStyle} lg:mx-[19%]`
         } ${divStyle} font-semibold text-3xl  sm:mx-10 mx-0 mt-4 mb-1 px-10`}
       >
         Results for <span className="text-secondary">{searchFeedPhrase}</span>
